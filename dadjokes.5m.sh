7853a1a99888b90735b131444f7b3110d9006ca4
Simon Peier <simon.peier@comerge.net>
2020-01-26 14:20:22 +0100

    Add dependency

--- dadjokes.5m.sh
+++ dadjokes.5m.sh
@@ -6,7 +6,7 @@
 # <bitbar.author.github>simonpeier</bitbar.author.github>
 # <bitbar.desc>The plugin tells you random dad jokes</bitbar.desc>
 # <bitbar.image>https://raw.githubusercontent.com/simonpeier/bitbar-dadjokes-plugin/master/screenshot.png</bitbar.image>
-# <bitbar.dependencies></bitbar.dependencies>
+# <bitbar.dependencies>bash</bitbar.dependencies>
 # <bitbar.abouturl>https://simonpeier.github.io/bitbar-dadjokes-plugin/</bitbar.abouturl>
 
 joke=$(curl -s -H "Accept: text/plain" https://icanhazdadjoke.com/)
